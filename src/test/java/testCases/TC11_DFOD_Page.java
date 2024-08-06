e2bfcd4f45212bd80ada3dd0475d44457dd5cb85
Ramdhiman007 <ramdhiman222@gmail.com>
2024-08-06 19:59:26 +0530

    addedUserManagementCases06-August-2024

--- src/test/java/testCases/TC11_DFOD_Page.java
+++ src/test/java/testCases/TC11_DFOD_Page.java
@@ -9,10 +9,7 @@ public class TC11_DFOD_Page extends pageObjects.Base_Page {
     @Test(priority=1)
     public void dfodpage() {
     	DFOD_Page lp = new DFOD_Page();
-        /*lp.dfodpage();
-        lp.dfodschedule("MANAGE SCHEDULES");
-        lp.dfodsRelay("CLOUD RELAYS");
-    } */
+        
 
     
 }
